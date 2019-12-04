22d511dff1aa241d334f6aeca63a7a2299083435
noobiwankenoobi <ross.hazelett@gmail.com>
2019-12-04 16:47:52 -0500

    changed all id in divs and buttons to classes

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,13 +1,15 @@
-const showCoffeePage = () => {
-    $('#main-page').hide()
-    $('#waffles-page').hide()
-    $('#wine-page').hide()
-    $('#press-page').hide()
-    $('#hours-page').hide()
-    $('#coffee-page').show()
-}
+'use strict'
 
-const addHandlers = () => {
-    $('#coffee-page-button').on('click', showCoffeePage)
+// const showCoffeePage = () => {
+//     $('#main-page').hide()
+//     $('#waffles-page').hide()
+//     $('#wine-page').hide()
+//     $('#press-page').hide()
+//     $('#hours-page').hide()
+//     $('#coffee-page').show()
+// }
 
-}
+// const addHandlers = () => {
+//     $('#coffee-page-button').on('click', showCoffeePage)
+
+// }
