299291d37cbadded88763e4f964cfb3d18f3fb5e
Ross Hazelett <rosshazelett@Rosss-MacBook-Pro-2.local>
2020-01-22 11:17:36 -0500

    all anchor link scrolling works now, refactored handler functions to run individual click functions for each part of page, made separate click functions for burger links so that closeNav doesn't run every time someone clicks a main nav link, social icons now link to a new tab instead of loading a new page in same tab, reordered some images and html

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,72 @@
 'use strict'
 
-// const clickEvents = require('./events.js')
+// SCROLL TO ANCHOR MAIN FUNCTION
+// SCROLL TO ANCHOR MAIN FUNCTION
 
-// NAV LINK CLICKS AND SCROLLS TO DIFFERENT PAGES
-// NAV LINK CLICKS AND SCROLLS TO DIFFERENT PAGES
-
-function scrollToAnchor(aid){
-  var aTag = $("a[name='"+ aid +"']");
+const scrollToAnchor =(anchorid) => {
+  var aTag = $("a[name='"+ anchorid +"']");
   $('html,body').animate({scrollTop: aTag.offset().top},'slow');
 }
 
+// SCROLL TO SPOTS ON PAGE FUNCTIONS USING MAIN NAV
+// SCROLL TO SPOTS ON PAGE FUNCTIONS USING MAIN NAV
+
+const scrollToHome = () => {
+  scrollToAnchor('home-page-anchor');
+}
+
+const scrollToCoffee = () => {
+    scrollToAnchor('coffee-page-anchor');
+  }
+
+const scrollToWaffles = () => {
+  scrollToAnchor('waffles-page-anchor');
+}
+
+const scrollToWine = () => {
+  scrollToAnchor('wine-page-anchor');
+}
+
+const scrollToPress = () => {
+  scrollToAnchor('press-page-anchor');
+}
+
+const scrollToHours = () => {
+  scrollToAnchor('hours-page-anchor');
+}
+
+// SCROLL TO SPOTS ON PAGE USING BURGER NAV
+// SCROLL TO SPOTS ON PAGE USING BURGER NAV
+
+const scrollToHomeBurger = () => {
+  scrollToAnchor('home-page-anchor');
+  closeNav()
+}
+
+const scrollToCoffeeBurger = () => {
+    scrollToAnchor('coffee-page-anchor');
+    closeNav()
+  }
+
+const scrollToWafflesBurger = () => {
+  scrollToAnchor('waffles-page-anchor');
+  closeNav()
+}
+
+const scrollToWineBurger = () => {
+  scrollToAnchor('wine-page-anchor');
+  closeNav()
+}
+
+const scrollToPressBurger = () => {
+  scrollToAnchor('press-page-anchor');
+  closeNav()
+}
+
+const scrollToHoursBurger = () => {
+  scrollToAnchor('hours-page-anchor');
+  closeNav()
+}
 
 
 // HAMBURGER NAV EVENTS
@@ -27,36 +84,25 @@ const closeNav = () => {
 // EVENT HANDLERS
 
 const addHandlers = () => {
-  // Nav bar scroll to anchor
-  $("#home-page-button").click(function() {
-    scrollToAnchor('home-page-anchor');
-   });
-  $("#coffee-page-button").click(function() {
-    scrollToAnchor('coffee-page-anchor');
-   });
-   $("#waffles-page-button").click(function() {
-    scrollToAnchor('waffles-page-anchor');
-   });
-   $("#wine-page-button").click(function() {
-    scrollToAnchor('wine-page-anchor');
-   });
-   $("#press-page-button").click(function() {
-    scrollToAnchor('press-page-anchor');
-   });
-   $("hours-page-button").click(function() {
-    scrollToAnchor('hours-page-anchor');
-   });
-
+  // Main Nav bar scroll to anchor
+  $("#home-page-button").on('click', scrollToHome) 
+  $('#coffee-page-button').on('click', scrollToCoffee)
+  $("#waffles-page-button").on('click', scrollToWaffles)
+  $("#wine-page-button").on('click', scrollToWine)
+  $("#press-page-button").on('click', scrollToPress)
+  $("#hours-page-button").on('click', scrollToHours)
+    
   // Burger menu open/close handlers
   $('.close-btn').on('click', closeNav)
   $('.open-btn').on('click', openNav)
+
   // Burger menu nav link handlers
-  $('.home-page-burger-button').on('click', showHomePage)
-  $('.coffee-page-burger-button').on('click', showCoffeePage)
-  $('.waffles-page-burger-button').on('click', showWafflesPage)
-  $('.wine-page-burger-button').on('click', showWinePage)
-  $('.press-page-burger-button').on('click', showPressPage)
-  $('.hours-page-burger-button').on('click', showHoursPage)
+  $('.home-page-burger-button').on('click', scrollToHomeBurger)
+  $('.coffee-page-burger-button').on('click', scrollToCoffeeBurger)
+  $('.waffles-page-burger-button').on('click', scrollToWafflesBurger)
+  $('.wine-page-burger-button').on('click', scrollToWineBurger)
+  $('.press-page-burger-button').on('click', scrollToPressBurger)
+  $('.hours-page-burger-button').on('click', scrollToHoursBurger)
 }
 
 // ON PAGE LOAD
